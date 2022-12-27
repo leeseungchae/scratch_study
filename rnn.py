6b2557d66d91beb21b78fbe9067e65d7c268c7d4
steve lee <steve.aitok@gmail.com>
2022-12-27 18:53:15 +0900

    [Feat] Add LSTM cell
    Resolves: [Feat] Add LSTM cell #2

--- rnn.py
+++ rnn.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple, Any
 
 import torch
 import torch.nn as nn
@@ -20,6 +20,7 @@ class RNNCellBase(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bias = bias
+        self.num_chunks =num_chunks
 
         self.ih = nn.Linear(in_features=input_size, out_features=hidden_size, bias=bias, **factory_kwargs)
         self.ih = nn.Linear(in_features=hidden_size, out_features=hidden_size, bias=bias, **factory_kwargs)
@@ -36,7 +37,7 @@ class RNNCell(RNNCellBase):
             dtype=None,
     ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
-        super().__init__()
+        super(RNNCell)
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.bias = bias
@@ -60,13 +61,38 @@ class RNNCell(RNNCellBase):
 
         return ret
 
-# class LSTMCell(RNNCellBase):
-#     def __init__(
-#             self,
-#             input_size: int,
-#             hidden_size: int,
-#             bias: bool,
-#             nonlinearity: str = 'tanh',
-#             device=None,
-#             dtype=None,
-#     ) -> None:
+class LSTMCell(RNNCellBase):
+    def __init__(
+            self,
+            input_size: int,
+            hidden_size: int,
+            bias: bool,
+            device=None,
+            dtype=None,
+    ) -> None:
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super(LSTMCell, self).__init__(input_size, 4*hidden_size,bias,num_chunks=4 , **factory_kwargs)
+
+    def forward(self, input: Tensor, hx: Optional[Tensor] = None) -> Tuple[Any, Any]:
+        if hx is None:
+            hx = torch.zeros(input.size(0), self.hidden_size, dtype=input.dtype, device=input.device)
+            hx = (hx,hx)
+
+        print(input.size())
+        print(self.ih.size())
+        print(self.hh.size())
+
+        hx, cx = hx
+        gates = self.ih(input) + self.hh(hx)
+        input_gate, forget_gate, cell_gate, output_gate = gates.chunk(4,1)
+
+        i_t = torch.sigmoid(input_gate)
+        f_t = torch.sigmoid(forget_gate)
+        g_t = torch.tanh(cell_gate)
+        o_t = torch.sigmoid(output_gate)
+
+        c_t = cx * f_t + i_t * g_t
+        h_t = o_t * torch.tanh(c_t)
+
+        return (h_t, c_t)
+
