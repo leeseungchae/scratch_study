eeb96d26dead7b554e5a5e0bef4a93b77b495f88
steve lee <steve.aitok@gmail.com>
2022-12-28 18:55:36 +0900

    [Style] Black style apply

--- rnn.py
+++ rnn.py
@@ -1,5 +1,6 @@
 import math
-from typing import Optional, Any
+from typing import Any, Optional
+
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -7,17 +8,19 @@ from torch import Tensor
 
 class RNNBase(nn.Module):
     """Base class for advanced RNN Module"""
-    __constants__ = ['input_size', 'output_size', 'bias']
+
+    __constants__ = ["input_size", "output_size", "bias"]
 
     def __init__(
-            self,
-            input_size: int,
-            hidden_size: int,
-            bias: bool,
-            num_chunks: int,
-            nonlinearity: str = 'tanh',
-            device=None,
-            dtype=None) -> None:
+        self,
+        input_size: int,
+        hidden_size: int,
+        bias: bool,
+        num_chunks: int,
+        nonlinearity: str = "tanh",
+        device=None,
+        dtype=None,
+    ) -> None:
         super().__init__()
         factory_kwargs = {"device": device, "dtype": dtype}
         self.input_size = input_size
@@ -25,19 +28,30 @@ class RNNBase(nn.Module):
         self.bias = bias
         self.num_chunks = num_chunks
         self.nonlinearity = nonlinearity
-        if self.nonlinearity not in ['tanh', 'relu']:
-            raise ValueError("Invalid nonlinearity selected for RNN. Can be either  ``tanh`` or ``relu``")
+        if self.nonlinearity not in ["tanh", "relu"]:
+            raise ValueError(
+                "Invalid nonlinearity selected for RNN. Can be either  ``tanh`` or ``relu``"
+            )
 
-        self.ih = nn.Linear(in_features=input_size, out_features=num_chunks*hidden_size, bias=bias, **factory_kwargs)
-        self.hh = nn.Linear(in_features=hidden_size, out_features=hidden_size, bias=bias, **factory_kwargs)
+        self.ih = nn.Linear(
+            in_features=input_size,
+            out_features=num_chunks * hidden_size,
+            bias=bias,
+            **factory_kwargs
+        )
+        self.hh = nn.Linear(
+            in_features=hidden_size,
+            out_features=hidden_size,
+            bias=bias,
+            **factory_kwargs
+        )
 
         self.reset_parameters()
 
-    def reset_parameters(self) ->None:
+    def reset_parameters(self) -> None:
         stdv = 1.0 / math.sqrt(self.hidden_size) if self.hidden_size > 0 else 0
         for weight in self.parameters():
             nn.init.uniform(weight, -stdv, stdv)
-
 
 
 # nn.RNN
@@ -53,28 +67,32 @@ class RNNCell(RNNBase):
     Inputs: input, hidden
         - input: tensor containing the input features
         - hidden: tensor containing the initial hidden state
-        """
+    """
 
     def __init__(
-            self,
-            input_size: int,
-            hidden_size: int,
-            bias: bool,
-            device=None,
-            dtype=None) -> None:
+        self, input_size: int, hidden_size: int, bias: bool, device=None, dtype=None
+    ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
-        super().__init__(input_size=input_size, hidden_size=hidden_size, bias=bias, num_chunks=1, **factory_kwargs)
+        super().__init__(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            bias=bias,
+            num_chunks=1,
+            **factory_kwargs
+        )
 
     def forward(self, input: Tensor, hx: Optional[Tensor] = None) -> Tensor:
         # Make zero tensor if tensor is not initialized
         if hx is None:
-            hx = torch.zeros(input.size(0), self.hidden_size, dtype=input.dtype, device=input.device)
+            hx = torch.zeros(
+                input.size(0), self.hidden_size, dtype=input.dtype, device=input.device
+            )
 
         # forward
         hy = self.ih(input) * self.hh(hx)
 
         # function
-        if self.nonlinearity == 'tanh':
+        if self.nonlinearity == "tanh":
             ret = torch.tanh(hy)
 
         else:
@@ -86,18 +104,18 @@ class RNNCell(RNNBase):
 # nn.LSTMCell
 class LSTMCell(RNNBase):
     def __init__(
-            self,
-            input_size: int,
-            hidden_size: int,
-            bias: bool,
-            device=None,
-            dtype=None) -> None:
+        self, input_size: int, hidden_size: int, bias: bool, device=None, dtype=None
+    ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
-        super(LSTMCell, self).__init__(input_size, hidden_size, bias=bias, num_chunks=4, **factory_kwargs)
+        super(LSTMCell, self).__init__(
+            input_size, hidden_size, bias=bias, num_chunks=4, **factory_kwargs
+        )
 
     def forward(self, input: Tensor, hx: Optional[Tensor] = None) -> Tensor:
         if hx is None:
-            hx = torch.zeros(input.size(0), self.hidden_size, dtype=input.dtype, device=input.device)
+            hx = torch.zeros(
+                input.size(0), self.hidden_size, dtype=input.dtype, device=input.device
+            )
             hx = (hx, hx)
 
         hx, cx = hx
@@ -118,18 +136,21 @@ class LSTMCell(RNNBase):
 # nn.GRU
 class GRUCell(RNNBase):
     def __init__(
-            self,
-            input_size: int,
-            hidden_size: int,
-            bias: bool,
-            device=None,
-            dtype=None) -> None:
+        self, input_size: int, hidden_size: int, bias: bool, device=None, dtype=None
+    ) -> None:
         factory_kwargs = {"device": device, "dtype": dtype}
-        super(GRUCell, self).__init__(input_size,  hidden_size, bias=bias, num_chunks=3, **factory_kwargs)
+        super(GRUCell, self).__init__(
+            input_size, hidden_size, bias=bias, num_chunks=3, **factory_kwargs
+        )
 
     def forward(self, input: Tensor, hx: Optional[Tensor] = None) -> Tensor:
         if hx is None:
-            hx = torch.zeros(input.size(0), self.num_chunks * self.hidden_size, dtype=input.dtype, device=input.device)
+            hx = torch.zeros(
+                input.size(0),
+                self.num_chunks * self.hidden_size,
+                dtype=input.dtype,
+                device=input.device,
+            )
 
         x_t = self.ih(input)
         h_t = self.hh(hx)
