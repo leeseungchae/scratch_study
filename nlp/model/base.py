92445d47926085ed0af2ed4a1fe28a44a0d1f064
steve lee <steve.aitok@gmail.com>
2023-01-04 19:18:06 +0900

    '[modify] RNNBase base.py '

--- nlp/model/base.py
+++ nlp/model/base.py
@@ -1,51 +1,96 @@
-import math
+import numbers
+import warnings
 
 import torch.nn as nn
 
+# class RNNBase(nn.Module):
+#     """Base class for advanced RNN Module"""
+#
+#     __constants__ = ["input_size", "output_size", "bias"]
+#
+#     def __init__(
+#         self,
+#         input_size: int,
+#         hidden_size: int,
+#         bias: bool,
+#         num_chunks: int,
+#         nonlinearity: str = "tanh",
+#         device=None,
+#         dtype=None,
+#     ) -> None:
+#         super().__init__()
+#         factory_kwargs = {"device": device, "dtype": dtype}
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.bias = bias
+#         self.num_chunks = num_chunks
+#         self.nonlinearity = nonlinearity
+#         if self.nonlinearity not in ["tanh", "relu"]:
+#             raise ValueError(
+#                 "Invalid nonlinearity selected for RNN. Can be either  ``tanh`` or ``relu``"
+#             )
+#
+#         self.ih = nn.Linear(
+#             in_features=input_size,
+#             out_features=num_chunks * hidden_size,
+#             bias=bias,
+#             **factory_kwargs
+#         )
+#         self.hh = nn.Linear(
+#             in_features=hidden_size,
+#             out_features=hidden_size,
+#             bias=bias,
+#             **factory_kwargs
+#         )
+#
+#         self.reset_parameters()
+#
+#     def reset_parameters(self) -> None:
+#         stdv = 1.0 / math.sqrt(self.hidden_size) if self.hidden_size > 0 else 0
+#         for weight in self.parameters():
+#             nn.init.uniform(weight, -stdv, stdv)
+#
+# class RNNcellBase(nn.Module):
+
 
 class RNNBase(nn.Module):
-    """Base class for advanced RNN Module"""
-
-    __constants__ = ["input_size", "output_size", "bias"]
-
     def __init__(
         self,
+        mode: str,
         input_size: int,
-        hidden_size: int,
-        bias: bool,
-        num_chunks: int,
-        nonlinearity: str = "tanh",
+        num_layres: int = 1,
+        bias: bool = True,
+        batch_first: bool = False,
+        dropout: float = 0,
+        bidirectional: bool = False,
         device=None,
         dtype=None,
     ) -> None:
         super().__init__()
-        factory_kwargs = {"device": device, "dtype": dtype}
+        self.mode = mode
         self.input_size = input_size
-        self.hidden_size = hidden_size
+        self.num_layers = num_layres
         self.bias = bias
-        self.num_chunks = num_chunks
-        self.nonlinearity = nonlinearity
-        if self.nonlinearity not in ["tanh", "relu"]:
+        self.batch_first = batch_first
+        self.dropout = nn.Dropout(p=float(dropout)) if float(dropout) != 0 else None
+        self.bidirectional = bidirectional
+        self.device = device
+        self.dtype = dtype
+        self.num_directions = 2 if bidirectional else 1
+
+        if not isinstance(dropout, numbers.Number) or not 0 <= dropout <= 1:
             raise ValueError(
-                "Invalid nonlinearity selected for RNN. Can be either  ``tanh`` or ``relu``"
+                "dropout should be a number in range[0,1]"
+                "representing the probability of an element being"
+                "zeroed"
+            )
+        if dropout >= 0 and num_layres == 1:
+            warnings.warn(
+                "dropout option adds dropout after all but last "
+                "recurrent layer, so non-zero dropout expects "
+                "num_layers greater than 1, but got dropout={} and "
+                "num_layers={}".format(dropout, num_layres)
             )
 
-        self.ih = nn.Linear(
-            in_features=input_size,
-            out_features=num_chunks * hidden_size,
-            bias=bias,
-            **factory_kwargs
-        )
-        self.hh = nn.Linear(
-            in_features=hidden_size,
-            out_features=hidden_size,
-            bias=bias,
-            **factory_kwargs
-        )
-
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-        stdv = 1.0 / math.sqrt(self.hidden_size) if self.hidden_size > 0 else 0
-        for weight in self.parameters():
-            nn.init.uniform(weight, -stdv, stdv)
+    def init_layers(self):
+        pass
--- nlp/model/rnn.py
+++ nlp/model/rnn.py
@@ -29,7 +29,7 @@ class RNNCell(RNNBase):
             hidden_size=hidden_size,
             bias=bias,
             num_chunks=1,
-            **factory_kwargs
+            **factory_kwargs,
         )
 
     def forward(self, input: Tensor, hx: Optional[Tensor] = None) -> Tensor:
@@ -50,3 +50,105 @@ class RNNCell(RNNBase):
             ret = torch.relu(hy)
 
         return ret
+
+
+class RNN(RNNBase):
+    def __init__(self, *args, **kwargs) -> None:
+        self.nonlinearity = kwargs.pop("nonlinearity", "tanh")
+        if self.nonlinearity == "tanh":
+            mode = "RNN_TAHN"
+        elif self.nonlinearity == "relu":
+            mode = "RNN_RELU"
+        else:
+            raise ValueError(f"UnKnown nonlinearity{self.nonlinearity}")
+        super(RNN, self).__init__(mode, *args, **kwargs)
+        self.forward_rnn = self.init_layers()
+
+    def forward(self, input: Tensor, hx: Optional[Tensor] = None) -> Tensor:
+
+        """
+
+        N :N batch_size
+        L : sequence length
+        D : 2 if bidrectional=True otherwise 1
+        H_in = input_size
+        H_out = hidden_size
+
+        :args
+            input :(Tensor) : The input can also be a packed variable length sequence.
+                -(L,H_in) for unbatched input
+                -(L,M,H_in) when ''batch_first'' = False
+                -(M,L,H_in) when ''batch_first'' = True
+            hx (Optional[Tensor], optional): _description_. Defaults to None.
+                -(num
+                -(num_layers, batch_size, H_out)
+        Returns
+            Tensor: _description_
+        """
+        batch_dim = 0 if self.batch_first else 1
+        is_batch = input.dim() == 3
+        if not is_batch:
+            input = input.unsqueeze(batch_dim)  # -> [1,L,H_in] or [L,1,H_in]
+            if hx is not None:
+                if hx.dim() != 2:
+                    raise RuntimeError(
+                        f"for batched 3-D input, hx should also be 3-D but got{hx.dim()}-D tensor"
+                    )
+                hx = hx.unsqueeze(1)  # -> [num_layers, 1, H_out]
+        else:
+            if (
+                hx is not None and hx.dim() != 3
+            ):  # hidden state를 넣었는데, dimension이 3개가 아닐 때
+                raise RuntimeError(
+                    f"for batched 3-D input, hx should also be 3-D but got{hx.dim()}-D tensor"
+                )
+
+            # input -> [N,L,H_in] or [L,H,H_in]
+            # hx -> [num_layers, batch_size, H_out]
+
+        batch_size = input.size(0) if self.batch_first else input.size(1)
+        sequence_size = input.size(1) if self.batch_first else input.size(1)
+
+        if hx is None:
+            hx = torch.zeros(
+                self.num_layers * self.num_directions,
+                batch_size,
+                self.hidden_size,
+                dtype=self.dtype,
+                device=self.device,
+            )
+
+        # hx -> [num_layers, batch_size, H_out]
+
+        hidden_state = []
+        next_hidden = []
+        for layer_idx, rnn_cell in enumerate(self.forward_rnn):
+            input_state = input  # Todo 여기가 if else 바꿔야지 layer 2부터 동작 가능
+            h_i = hx[
+                layer_idx, :, :
+            ]  # -> [layer_idx번째, batch_szie, H_out] layer_idx번째의 hidden_state
+
+            for i in range(sequence_size):
+                input_split = (
+                    input_state[:, i, :] if self.batch_first else input_state[i, :, :]
+                )
+                # -> [N,1,H_in] or [1,N,H_in]
+                h_i = rnn_cell(input_split, h_i)
+                # Todo : dropout
+                next_hidden.append(h_i)
+            hidden_state.append(torch.stack(next_hidden, dim=1))
+
+    def init_layers(self):
+        layers = []
+        for layer_idx in range(self.num_layers):
+            input_size = self.input_size if layer_idx == 0 else self.hidden_size
+            layers.append(
+                RNNCell(
+                    input_size=input_size,
+                    hidden_size=self.hidden_size,
+                    bias=self.bias,
+                    nonlinearity=self.nonlinearity,
+                    device=self.device,
+                    dtype=self.dtype,
+                )
+            )
