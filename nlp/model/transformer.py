4805111bbf0acd58edfc87bc8ab99ba528d736ee
lee seungchae <steve.aitok@gmail.com>
2023-05-24 13:07:28 +0900

    [Feat] Add Attention

--- nlp/model/transformer.py
+++ nlp/model/transformer.py
@@ -1,10 +1,9 @@
+import math
 from typing import Tuple
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import math
-
 from torch import Tensor
 from torch.autograd import Variable
 
@@ -15,7 +14,7 @@ class Scaled_dot_product_Attention(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
 
     def forward(
-        self, query: Tensor, key: Tensor, value: Tensor,mask=None ,dropout=None
+        self, query: Tensor, key: Tensor, value: Tensor, mask=None, dropout=None
     ) -> Tuple[Tensor, Tensor]:
         """dot product attention
 
@@ -28,17 +27,19 @@ class Scaled_dot_product_Attention(nn.Module):
             _type_: _description_
             :param mask:
         """
-        #https://cpm0722.github.io/pytorch-implementation/transformer
+        # https://cpm0722.github.io/pytorch-implementation/transformer
         # 벡터의 차원을 얻음
         d_k = query.size(-1)
         print(d_k)
-        scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(torch.tensor(d_k, dtype=torch.float32))
+        scores = torch.matmul(query, key.transpose(-2, -1)) / torch.sqrt(
+            torch.tensor(d_k, dtype=torch.float32)
+        )
 
         if mask is not None:
             scores = scores.masked_fill(mask == 0, -1e9)
 
         attention_weights = F.softmax(scores, dim=-1)
-
+        print("test")
         if dropout is not None:
             attention_weights = dropout(attention_weights)
 
@@ -53,9 +54,11 @@ class Scaled_dot_product_Attention(nn.Module):
         print(f"scores {scores.size()}")
         return context, attn_prob
 
-class PositionalEncoding(nn.Module):
-    def __init__(self, hidden_dim: int = 512, dropout: float = 0.0, max_len: int = 5000):
 
+class PositionalEncoding(nn.Module):
+    def __init__(
+        self, hidden_dim: int = 512, dropout: float = 0.0, max_len: int = 5000
+    ):
         # https://wikidocs.net/31379
         # https://sirzzang.github.io/ai/AI-Transformer-02/
         # mex_len 논문 기준
@@ -64,11 +67,14 @@ class PositionalEncoding(nn.Module):
 
         # 벡터 초기화 (벡터간 거리 일정)
         pe = torch.zeros(max_len, hidden_dim)  # torch.Size([5000, 512])
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)  # torch.Size([5000, 1])
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(
+            1
+        )  # torch.Size([5000, 1])
 
         # torch.exp: 자연상수, log: 자연 로그, 홀수 인덱스의 경우 cos함수, 짝수 인덱스의 경우 sin함수 주기
-        div_term = torch.exp((torch.arange(0, hidden_dim, 2) *
-                              -(math.log(10000.0) / hidden_dim)).float())  # torch.Size([256])
+        div_term = torch.exp(
+            (torch.arange(0, hidden_dim, 2) * -(math.log(10000.0) / hidden_dim)).float()
+        )  # torch.Size([256])
 
         # sin,cos 함수를 이용하기 때문에, 모두 -1 ~ 1 사이로 통일 벡터가 발산하지 않는다.
         pe[:, 0::2] = torch.sin(position.float() * div_term)  # torch.Size([5000, 512])
@@ -77,7 +83,7 @@ class PositionalEncoding(nn.Module):
         pe = pe.unsqueeze(0)  # torch.Size([1, 5000, 512])
 
         # pe를 모델의 버퍼로 등록 (파타미터로 학습 안되게, 역전파에 의해 변경되지 않는다,)
-        self.register_buffer('pe', pe)
+        self.register_buffer("pe", pe)
 
     # 입력 데이터에 PositionalEncoding 추가하는 과정
 
@@ -92,9 +98,8 @@ class PositionalEncoding(nn.Module):
         x = self.embbedding(x)
         x = x * math.sqrt(self.dim)
         if step is None:
-            x = x + self.pe[:, :x.size(1)]
+            x = x + self.pe[:, : x.size(1)]
         else:
             x = x + self.pe[:, step]
         x = self.dropout(x)
         return x
-
